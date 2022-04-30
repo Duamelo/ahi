ee717be0668072a6d1125eb026018c033023ddd7
Duamelo <david.dosseh@imsp-uac.org>
2022-05-01 01:01:03 +0200

    implemented architecture

---
+++ server/src/infrastructure/src/persistence/postgres/EntityGateway.ts
@@ -0,0 +1,7 @@
+// Layer 1 (yellow) dependencies from domain
+
+
+// Lives in Layer 4 (blue) in infrastructure
+// export class EntityGateway implements IEntityGateway {
+   
+// }
---
+++ server/src/infrastructure/src/utils/validateEnv.ts
@@ -0,0 +1,14 @@
+import { cleanEnv, port, str } from "envalid";
+  
+function validateEnv() {
+    cleanEnv(process.env, {
+      JWT_SECRET: str(),
+      POSTGRES_HOST: str(),
+      POSTGRES_PORT: port(),
+      POSTGRES_USER: str(),
+      POSTGRES_PASSWORD: str(),
+      POSTGRES_DB: str(),
+      PORT: port(),
+    });
+  }
+export default validateEnv;
