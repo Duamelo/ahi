f55f801a82a1607b837648279e8e86cea028b2a1
ruchdane <ruchdaneabiodun@gmail.com>
2022-07-03 10:28:01 +0100

    implementation for category

---
+++ api/mock/error/category.js
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+
+export function create(form, callback, error) {
+    error({error:"Cette catégorie existe deja"});
+}
+
+
+export function get(query, callback, error) {
+    if (typeof query == "number") {        
+        error({error:"Cette catégorie n'existe pas"});
+        return
+    }
+    error({error:"Cette catégorie n'existe pas"});
+}
+
+export function put(id,form, callback, error) {
+    error({error:"Cette catégorie n'existe pas",id:id});
+}
+
+export function remove(id, callback, error) {
+    error({error:"Cette catégorie n'existe pas",id:id});
+}
